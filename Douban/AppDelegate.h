1276052667c5771a2a6ea6e77af034135d8fbdbe
demonjz <demon890128@gmail.com>
2015-05-05 15:13:19 +0800

    Initial Commit

---
+++ Douban/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Douban
+//
+//  Created by demon_Z on 15/5/5.
+//  Copyright (c) 2015年 demon_Z. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Douban/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Douban
+//
+//  Created by demon_Z on 15/5/5.
+//  Copyright (c) 2015年 demon_Z. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
